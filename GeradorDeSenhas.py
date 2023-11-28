1fe4abcbe2abf01f0f3fd342e144339055eab7cb
Felipe Caldeira <146666806+Ferpix01@users.noreply.github.com>
2023-11-28 15:08:33 -0300

    Add files via upload

---
+++ GeradorDeSenhas.py
@@ -0,0 +1,13 @@
+import random  # importa a biblioteca random
+
+senha = "" #define a senha como uma string sem nada
+
+letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&*?çÇ" # todas os caracteres para combinação
+
+for digito in range (15): # declara que a senha terá 15 caracteres
+    senhaaleatoria = random.choice(letras)
+    senha += senhaaleatoria # joga a senha gerada na string vazia
+print("-" * 25)
+print(senha)
+print("-" * 25)
+print("Esta é sua senha.")
